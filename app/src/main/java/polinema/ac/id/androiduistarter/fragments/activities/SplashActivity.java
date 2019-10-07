c2844fa2e823075ede081b5d8a42a69a6acfaa1c
reinatrychadani <trycha67@gmail.com>
2019-10-07 14:47:16 +0700

    menyelesaikan praktikum View pager

--- app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/SplashActivity.java
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/SplashActivity.java
@@ -15,7 +15,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable(){
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(SplashActivity.this,WelcomeActivity.class));
